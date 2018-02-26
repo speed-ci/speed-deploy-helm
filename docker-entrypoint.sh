8cb5ab96b0f3dba3282c0d9148434da1884f2cfd
mathieu.filotto <m.filotto@eramet-sln.nc>
2018-02-26 12:58:13 +1100

    feat(cleanup): improve cleanup of previsou failed release

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -81,7 +81,12 @@ if [[ `helm ls --failed | grep $RELEASE` ]]; then
     REVISION_COUNT=`helm history $RELEASE | tail -n +2 | wc -l`
     if [[ $REVISION_COUNT == 1 ]]; then
         printinfo "Suppression du premier déploiement en erreur"
+        printcomment "helm delete --purge $RELEASE"
         helm delete --purge $RELEASE 
+    elif [[ `helm history $RELEASE | tail -n 1 | grep FAILED` ]]; then
+        printinfo "Suppression du dernier déploiement en erreur"
+        printcomment "helm delete $RELEASE"
+        helm delete $RELEASE 
     fi
 fi
 
