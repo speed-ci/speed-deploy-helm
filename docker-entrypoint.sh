f9346cce92a93941be28514ae532d9fb5f833597
Mathieu Filotto <m.filotto@eramet-sln.nc>
2019-04-19 09:01:35 +1100

    feat(env): move kubectl env config in base helm image

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,7 @@
 set -e
 
 source /init.sh
+source /init_helm.sh
 
 function help () {
 # Using a here doc with standard out.
@@ -102,32 +103,7 @@ done
 printmainstep "Déploiement de l'application avec Kubernetes Helm"
 printstep "Vérification des paramètres d'entrée"
 init_artifactory_env
-
-KUBECONFIG_ORIGIN_FOLDER="/srv/kubeconfig"
-KUBECONFIG_FOLDER="/root/.kube"
-KUBECONFIG_DEFAULT_PATH="$KUBECONFIG_FOLDER/config"
-KUBECONFIG_OVERRIDE_PATH="$KUBECONFIG_FOLDER/config.override"
-mkdir -p $KUBECONFIG_FOLDER
-cp $KUBECONFIG_ORIGIN_FOLDER/*config* $KUBECONFIG_FOLDER
-
-if [[ $KUBECONFIG_OVERRIDE ]]; then
-    echo $KUBECONFIG_OVERRIDE | yq r - > $KUBECONFIG_OVERRIDE_PATH
-fi
-
-KUBECONFIG=""
-for i in $KUBECONFIG_FOLDER/*config*; do
-    KUBECONFIG="$KUBECONFIG:$i"
-done
-
-export KUBECONFIG=$KUBECONFIG
-printinfo "KUBECONFIG                   : $KUBECONFIG"
-KUBECONTEXT_LIST=`kubectl config get-contexts -o name`
-if [[ -z $KUBECONTEXT_LIST ]]; then
-    printerror "Aucun context kubernetes trouvé: la configuration d'accès au cluster kubernetes doit être renseignée (on recherche des fichiers contenant le mot clef config)"
-    printerror "- soit en montant et associant le volume $KUBECONFIG_FOLDER au container (ex: -v ~/.kube:$KUBECONFIG_FOLDER)"
-    printerror "- soit en renseignant la variable d'environnement KUBECONFIG_OVERRIDE (les guillements doivent être échappés)"
-    exit 1
-fi  
+init_helm_env
 
 CHART_FILE_NAME="Chart.yaml"
 if [ ! -f $CHART_FILE_NAME ]; then
@@ -139,36 +115,6 @@ CHART_NAME=$(yq r Chart.yaml name)
 if [ $CHART_NAME == null ]; then
     printerror "Le fichier de meta-données $CHART_FILE_NAME doit contenir le nom du chart (champ name)"
     exit 1
-fi
-
-declare -A KUBE_CONTEXT_MAPPING_RULES
-if [[ $BRANCH_KUBE_CONTEXT_MAPPING  ]]; then
-    while read name value; do
-        KUBE_CONTEXT_MAPPING_RULES[$name]=$value
-    done < <(<<<"$BRANCH_KUBE_CONTEXT_MAPPING" awk -F= '{print $1,$2}' RS=',|\n')
-fi
-
-declare -A NAMESPACE_MAPPING_RULES
-NAMESPACE_MAPPING_RULES[master]=default
-if [[ $BRANCH_NAMESPACE_MAPPING  ]]; then
-    while read name value; do
-        NAMESPACE_MAPPING_RULES[$name]=$value
-    done < <(<<<"$BRANCH_NAMESPACE_MAPPING" awk -F= '{print $1,$2}' RS=',|\n')
-fi
-
-if [[ -z $BRANCH_NAME ]]; then
-  if [[ ! $(git status | grep "Initial commit")  ]]; then
-    BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
-  fi
-  BRANCH_NAME=${BRANCH_NAME:-"master"}
-fi
-
-if [[ -z $KUBE_CONTEXT ]]; then
-   DEFAULT_KUBE_CONTEXT=$(kubectl config current-context 2>/dev/null || echo "")
-   KUBE_CONTEXT=${KUBE_CONTEXT_MAPPING_RULES[$BRANCH_NAME]:-$DEFAULT_KUBE_CONTEXT}
-fi
-if [[ -z $NAMESPACE ]]; then
-   NAMESPACE=${NAMESPACE_MAPPING_RULES[$BRANCH_NAME]:-$BRANCH_NAME}
 fi
 
 if [[ -z $SKIP_INIT ]]; then
@@ -186,15 +132,12 @@ printinfo "KUBE_CONTEXT                : $KUBE_CONTEXT"
 printinfo "NAMESPACE                   : $NAMESPACE"
 printinfo "RELEASE                     : $RELEASE"
 printinfo "TIMEOUT                     : $TIMEOUT"
+printinfo "KUBECONFIG                  : $KUBECONFIG"
 printinfo "BRANCH_KUBE_CONTEXT_MAPPING : $BRANCH_KUBE_CONTEXT_MAPPING"
 printinfo "BRANCH_NAMESPACE_MAPPING    : $BRANCH_NAMESPACE_MAPPING"
 printinfo "INGRESS_TLS_SECRET          : $INGRESS_TLS_SECRET"
 printinfo "INGRESS_TLS_NAMESPACE       : $INGRESS_TLS_NAMESPACE"
 printinfo "SKIP_INIT                   : $SKIP_INIT"
-
-printstep "Définition du contexte Kubernetes par défaut"
-printcomment "kubectl config use-context $KUBE_CONTEXT"
-kubectl config use-context $KUBE_CONTEXT
 
 printstep "Création du namespace $NAMESPACE"
 if ! kubectl get ns --ignore-not-found | grep -q $NAMESPACE ; then
